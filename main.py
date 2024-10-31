54c5773b9b644f61c8505e19149fe285f4b21885
gruzd <gruzdev-2002@bk.ru>
2024-10-31 18:11:37 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import math
+
+
+def find_roots(a, b, c):
+    if a == b == c == 0:
+        raise ValueError("Все коэффициенты равны нулю")
+
+    try:
+        discriminant = b ** 2 - 4 * a * c
+        if discriminant < 0:
+            return None, None
+        elif discriminant == 0:
+            root = -b / (2 * a)
+            return root, root
+        else:
+            sqrt_discriminant = math.sqrt(discriminant)
+            root1 = (-b + sqrt_discriminant) / (2 * a)
+            root2 = (-b - sqrt_discriminant) / (2 * a)
+            return root1, root2
+    except ZeroDivisionError:
+        return None, None
+
+
+
---
+++ unit_tests.py
@@ -0,0 +1,62 @@
+import unittest
+from main import find_roots
+import math
+
+
+class TestFindRoots(unittest.TestCase):
+
+    def test_two_real_roots(self):
+        """Тест, когда уравнение имеет два различных действительных корня"""
+        a, b, c = 1, -3, 2
+        roots = find_roots(a, b, c)
+        self.assertIsNotNone(roots)
+        self.assertNotEqual(roots[0], roots[1])
+        self.assertAlmostEqual(roots[0] * roots[1], c / a, delta=1e-9)
+        self.assertAlmostEqual(roots[0] + roots[1], -b / a, delta=1e-9)
+
+    def test_one_real_root(self):
+        """Тест, когда уравнение имеет один действительный корень"""
+        a, b, c = 1, 0, 1
+        roots = find_roots(a, b, c)
+        self.assertIsNotNone(roots)
+        self.assertEqual(len(roots), 2)
+        self.assertEqual(roots[0], roots[1])
+        if roots[0] is not None:
+            self.assertAlmostEqual(roots[0] ** 2 + b * roots[0] + c, 0, places=6)
+        else:
+            self.assertIsNone(roots[0])
+            self.assertIsNone(roots[1])
+
+    def test_integer_coefficients(self):
+        """Тест, когда коэффициенты a, b, c - целые числа"""
+        a, b, c = 1, -2, 1
+        roots = find_roots(a, b, c)
+        self.assertIsNotNone(roots)
+        self.assertEqual(roots[0], roots[1])
+        self.assertAlmostEqual(roots[0] ** 2 + b * roots[0] + c, 0, delta=1e-9)
+
+    def test_float_coefficients(self):
+        """Тест, когда коэффициенты a, b, c - вещественные числа"""
+        a, b, c = 1.5, 2.3, -1.0
+        roots = find_roots(a, b, c)
+
+        self.assertTrue(math.isclose(roots[0] ** 2 + b * roots[0] + c, 0, rel_tol=1e-9))
+        self.assertTrue(math.isclose(roots[1] ** 2 + b * roots[1] + c, 0, rel_tol=1e-9))
+
+    def test_negative_discriminant(self):
+        """Тест, когда дискриминант отрицательный (нет действительных корней)"""
+        a, b, c = 1, 1, 1
+        roots = find_roots(a, b, c)
+        self.assertIsNotNone(roots)
+        self.assertIsNone(roots[0])
+        self.assertIsNone(roots[1])
+
+    def test_all_zero_coefficients(self):
+        """Тест, когда все коэффициенты равны 0"""
+        a, b, c = 0, 0, 0
+        with self.assertRaises(ValueError):
+            find_roots(a, b, c)
+
+
+if __name__ == '__main__':
+    unittest.main()
